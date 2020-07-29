136d1f28bfd40363e2151637510b22fa3186b18a
Franco Vitale <franco.vitalematus@gmail.com>
2020-07-29 04:37:22 -0400

    Modelos y Entidades con Doctrine: Creación Ent y Modelos, Pruebas

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,15 +3,32 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Entity\Video;
 
 class UserController extends AbstractController
 {    
     
     public function index()
     {
+        $user_repo = $this->getDoctrine()->getRepository(User::class);
+        $video_repo = $this->getDoctrine()->getRepository(Video::class);
+
+        $users = $user_repo->findAll();
+
+        foreach($users as $user){
+            echo "<h1>{$user->getName()} {$user->getSurname()}</h1>";
+
+            foreach($user->getVideos() as $video){
+                echo "<p>{$video->getTitle()} - {$video->getUser()->getEmail()}</p>";
+            }
+        }
+
+        die();    
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/UserController.php',
         ]);
+    
     }
 }
